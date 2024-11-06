c9a34f2d4cc05d59f3e27fd67fcdf39166ea9ac6
inespanol <inespanol2020@mail.ru>
2024-11-06 20:39:56 +0300

    ДЗ2

---
+++ script.js
@@ -0,0 +1,66 @@
+let a = 10; // задание 1
+alert(a);
+a = 20;
+alert(a);
+
+let iphoneYear = 2022; // задание 2
+alert(iphoneYear);
+
+let creatorJs = 'Брендан Эйх'; // задание 3
+alert(creatorJs);
+
+let b = 10; // задание 4
+let c = 2;
+alert(a + b); 
+alert(a - b); 
+alert(a * b); 
+alert(a / b); 
+
+let result = 2 ** 5; // задание 5
+alert(result);
+
+let d = 9; // задание 6
+let e = 2;
+alert(a % b); 
+
+let num = 1; // задание 7
+num += 5; 
+num -= 3; 
+num *= 7; 
+num /= 3; 
+num++; 
+num--; 
+alert(num);
+
+let age = prompt("Сколько вам лет?"); // задание 8
+alert(age);
+
+let user = { // задание 9
+    name: 'Сергей',
+    age: 54,
+    isAdmin: true
+  }; 
+  alert(user.name); 
+  alert(user.age); 
+  alert(user.isAdmin); 
+
+let userName = prompt("Как вас зовут?"); // задание 10
+alert("Привет, " + userName + "!");
+
+// Дополнительно
+let number = prompt("Загадайте любое число");
+
+let doubledNumber = 2 * number;
+alert(doubledNumber);
+
+let addedNumber = doubledNumber + 10;
+alert(addedNumber);
+
+let dividedNumber = addedNumber / 2;
+alert(dividedNumber);
+
+let subtractedNumber = dividedNumber - number;
+alert(subtractedNumber);
+
+alert("Ответ равен 5");
+
